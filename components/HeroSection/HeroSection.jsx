acf15c8c233ad82ad5c3e508d38e0d4c6ce82d77
BhavishaChafekar <bhavishachafekar@gmail.com>
2024-02-11 03:26:42 +0530

    finall

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -11,9 +11,9 @@ const HeroSection = () => {
     <div className={Style.heroSection}>
       <div className={Style.heroSection_box}>
         <div className={Style.heroSection_box_left}>
-          <h1>Discover, collect, and sell NFTs 🖼️</h1>
+          <h1>Unlock NFT Wonders</h1>
           <p>
-            Discover the most outstanding NTFs in all topics of life. Creative
+          Your Gateway to a Creative Cosmos. Discover the most outstanding NTFs in all topics of life. Creative
             your NTFs and sell them
           </p>
           <Button btnName="Start your search" />
